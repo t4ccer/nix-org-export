d8bb2b2ed0c6e9702b95ffe6a74f6e76d1303cfc
t4ccer <t4ccer@gmail.com>
2023-02-20 20:11:42 -0700

    Add `org2pdf` app

--- flake.nix
+++ flake.nix
@@ -35,7 +35,9 @@
           (message "Exporting org to pdf...")
           (require 'org)
           (find-file (getenv "NIX_ORG_EXPORT_FILE"))
-          (copy-file (org-latex-export-to-pdf) (getenv "NIX_ORG_EXPORT_OUT") 't)
+          (if (getenv "NIX_ORG_EXPORT_OUT")
+            (copy-file (org-latex-export-to-pdf) (getenv "NIX_ORG_EXPORT_OUT") 't)
+            (org-latex-export-to-pdf))
           (message "Exporting org to pdf done!")
         '';
       };
@@ -44,20 +46,36 @@
       (pkgsFor system).stdenv.mkDerivation {
         name = "exported.pdf";
         inherit src;
-        buildInputs = [
-          (pkgsFor system).texlive.combined.scheme-full
-        ];
         buildPhase = ''
           cp -r $src .
           stat ${file}
-          export NIX_ORG_EXPORT_FILE="${file}"
           export NIX_ORG_EXPORT_OUT="$(mktemp out.XXXXXX)"
-          ${customEmacsFor system}/bin/emacs --batch --no-init --load ${elispScriptFor system}
+          ${self.apps.${system}.org2pdf.program} "${file}"
         '';
         installPhase = ''
           cp $NIX_ORG_EXPORT_OUT $out
         '';
       };
+
+    apps = perSystem (system: {
+      default = self.apps.${system}.org2pdf;
+      org2pdf = {
+        type = "app";
+        program =
+          ((pkgsFor system).writeShellScript "org2pdf.sh" ''
+            export PATH="${(pkgsFor system).texlive.combined.scheme-full}/bin:${(pkgsFor system).git}/bin"
+            if (($# == 1))
+            then
+            export NIX_ORG_EXPORT_FILE="$1"
+            ${customEmacsFor system}/bin/emacs --batch --no-init --load ${elispScriptFor system}
+            else
+            echo "Usage: $0 <org-file>"
+            exit 1
+            fi
+          '')
+          .outPath;
+      };
+    });
 
     # Example how to use `exportOrgFor`
     readmeExported = perSystem (system: exportOrgFor system ./. "README.org");
